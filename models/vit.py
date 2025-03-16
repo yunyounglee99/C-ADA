1182e65aba156009f1526d96c5115632d2b300cd
nyoung <kembel0116@gmail.com>
2025-03-17 01:30:37 +0900

    .

--- models/vit.py
+++ models/vit.py
@@ -6,14 +6,18 @@ from models.adapter_layer import ContinualAdapterLayer
 from models.scaling_and_shifting import ScailingAndShifting
 
 class ViTBlockWithCADA(nn.Module):
-  def __init__(self, original_block, embed_dim, hidden_dim, total_tasks):
+  def __init__(self, original_block, embed_dim, hidden_dim):
     super().__init__()
     self.original_block = original_block
-    self.cal_msha = ContinualAdapterLayer(embed_dim, hidden_dim, total_tasks)
-    self.cal_mlp = ContinualAdapterLayer(embed_dim, hidden_dim, total_tasks)
+    self.cal_msha = ContinualAdapterLayer(embed_dim, hidden_dim)
+    self.cal_mlp = ContinualAdapterLayer(embed_dim, hidden_dim)
     self.lamda = 0.1    # 일부러 스펠링 틀린것
     self.sns = ScailingAndShifting(embed_dim)
     self.sns_frozen = False
+
+  def add_new_task(self):
+    self.cal_msha.add_new_task()
+    self.cal_mlp.add_new_task()
 
   def set_current_task(self, task_id):
     self.cal_msha.set_current_task(task_id)
@@ -63,7 +67,7 @@ class CADA_ViTModel(nn.Module):
               num_classes,
               model_name = "google/vit-base-patch16-224-in21k"):
     super().__init__()
-    self.config = ViTConfig
+    self.config = ViTConfig.from_pretrained(model_name)
     embed_dim = self.config.hidden_size
 
     self.base_vit = ViTModel.from_pretrained(model_name)
@@ -76,13 +80,15 @@ class CADA_ViTModel(nn.Module):
         original_block=block,
         embed_dim=embed_dim,
         hidden_dim=hidden_dim,
-        total_tasks=total_tasks
       )
       self.base_vit.encoder.layer[i] = wrapped_block
 
-    self.total_tasks = total_tasks
-    self.current_task = 0
     self.classifier = nn.Linear(embed_dim, num_classes)
+    self.current_task = None
+
+  def add_new_task(self):
+    for block in self.base_vit.encoder.layer:
+      block.add_new_task()
 
   def set_current_task(self, task_id):
     self.current_task = task_id
