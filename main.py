0487ee23b0350f0afdc9b5487d4a3fe7e97d5762
nyoung <kembel0116@gmail.com>
2025-03-22 15:23:17 +0900

    .

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ import torchvision.transforms as transforms
 from models.orthogonal_loss import orthogonal_loss
 from models.vit import CADA_ViTModel, ViTBlockWithCADA
 
+transform_train = transforms.Compose([
+  transforms.Resize((224,224)),
+  transforms.RandomHorizontalFlip(),
+  transforms.ToTensor(),
+  transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, ))
+])
+
 train_dataset = torchvision.datasets.CIFAR100(
   root = "./data",
   train = True,
