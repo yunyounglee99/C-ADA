cb62d329b176a82853eaea90c78ee2749b673501
nyoung <kembel0116@gmail.com>
2025-03-22 16:05:30 +0900

    .

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import torch
+import yaml
+import argparse
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision 
@@ -6,6 +8,16 @@ import torchvision.transforms as transforms
 
 from models.orthogonal_loss import orthogonal_loss
 from models.vit import CADA_ViTModel, ViTBlockWithCADA
+from train.incremental_train import train_incremental
+
+def load_config(config_path):
+  with open(config_path, "r") as f:
+    return yaml.safe_load(f)
+  
+parser = argparse.ArgumentParser()
+parser.add_argument("--config,", type = str, default = "config.yaml")
+args = parser.parse_args()
+config = load_config(args.config)
 
 transform_train = transforms.Compose([
   transforms.Resize((224,224)),
@@ -32,9 +44,19 @@ test_dataset = torchvision.datasets.CIFAR100(
   transform = transform_test
 )
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, num_workers=2)
+test_loader = DataLoader(test_dataset, batch_size=config['batch_size'], shuffle=False, num_workers=2)
 
 model = CADA_ViTModel(
-  hidden_dim = hidden_dim,
+  hidden_dim = config['middle_dim'],
+  num_classes = config['num_classes'],
+  model_name="google/vit-base-patch16-224-in21"
+)
+train_incremental(
+  model = model,
+  train_loader = train_loader,
+  task_id = 1,
+  num_epochs = config['num_epochs'],
+  lr = config['lr'],
+  alpha = config['lr']
 )
