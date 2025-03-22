b5ea2d2decc0c5ba9de52eea463fafbe12d19220
nyoung <kembel0116@gmail.com>
2025-03-22 15:31:56 +0900

    .

--- main.py
+++ main.py
@@ -11,12 +11,30 @@ transform_train = transforms.Compose([
   transforms.Resize((224,224)),
   transforms.RandomHorizontalFlip(),
   transforms.ToTensor(),
-  transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, ))
+  transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
+])
+transform_test = transforms([
+  transforms.Resize((224, 224)),
+  transforms.ToTensor(),
+  transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
 ])
 
 train_dataset = torchvision.datasets.CIFAR100(
   root = "./data",
   train = True,
   download = True,
-  transform = 
+  transform = transform_train
+)
+test_dataset = torchvision.datasets.CIFAR100(
+  root = "./data",
+  train = False,
+  download = True,
+  transform = transform_test
+)
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+model = CADA_ViTModel(
+  hidden_dim = hidden_dim,
 )
