7b6b89a3ba3369c3cc0d0e4dbde62273af6a1261
nyoung <kembel0116@gmail.com>
2025-03-24 23:25:04 +0900

    .

--- models/adapter_layer.py
+++ models/adapter_layer.py
@@ -20,12 +20,12 @@ class ContinualAdapterLayer(nn.Module):
       for param in self.up_projections[i].parameters():
         param.requires_grad = False
 
-    device = next(self.parameters()).device
     down = nn.Linear(self.in_dim, self.hidden_dim, bias = False)
     up = nn.Linear(self.hidden_dim, self.in_dim, bias = False)
 
-    self.down_projections.append(down)
-    self.up_projections.append(up)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    self.down_projections.append(down).to(device)
+    self.up_projections.append(up).to(device)
 
     new_task_id = len(self.down_projections)-1
     self.current_task = new_task_id
@@ -49,9 +49,21 @@ class ContinualAdapterLayer(nn.Module):
     if self.current_task is None:
       raise ValueError("No task has been added yet. add_new_task() first")
     
-    down_w = self.down_projections[self.current_task]
-    up_w = self.up_projections[self.current_task]
+    if x.dim() == 3:
+      B, S, D = x.shape
+      T = len(self.down_projections)
+      x = x.unsqueeze(1).expand(-1, T, -1, -1)
+    elif x.dim() == 4:
+      T = len(self.down_projections)
 
-    out = self.relu(down_w(x))
-    out = up_w(out)
+    else:
+      raise ValueError(f"unexpected input shape : {x.shape}")
+    
+    W_dp = torch.stack([p.weight for p in self.down_projections], dim = 0)      #(T, D, d)
+    W_up = torch.stack([p.weight for p in self.up_projections], dim = 0)     #(T, d, D)
+
+    out = torch.einsum('btsd, tdh -> btsh', x, W_dp)
+    out = self.relu(out)
+    out = torch.einsum('btsh, thd -> btsh', out, W_up)
+  
     return out
